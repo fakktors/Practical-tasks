f1f497f38c5168c642b31be656419b649e8a2b0a
Evgenii S <fakktorss@gmail.com>
2021-11-20 12:17:42 +0300

    Done 1st Practical task

---
+++ MaxUniqCharacters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace MaxUniqCharacters
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Создайте программу, которая принимает последовательность символов (строку)
+        /// в качестве аргументов из командной строки
+        /// и выводит на консоль максимальное количество неравных последовательных символов в строке.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DistinctUserInput(null));
+            Console.ReadKey();
+        }
+
+        static string DistinctUserInput(string uniqChars)
+        {
+            // Читаем ввод пользователя
+            uniqChars = Console.ReadLine();
+            // Используем Linq для вычисления уникальных значений
+            var distinctChasrs = uniqChars.Distinct().Count();
+            // Возвращает кол-во уникальных значений
+            return distinctChasrs.ToString();
+        }
+    }
+}
